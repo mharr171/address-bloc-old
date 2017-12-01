d8905e99300f63e21aba39864ff1a2e3163fe0b2
Matthew Harris <mharr171.z@gmail.com>
2017-12-01 01:48:12 -0800

    assignment-7: complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -42,14 +42,14 @@ class AddressBook
 
   def binary_search name
     lower = 0
-    upper = entries.length - 1
+    upper = @entries.length - 1
 
     while lower <= upper
       mid = (lower + upper) / 2
-      mid_name = entries[mid].name
+      mid_name = @entries[mid].name
 
       if name == mid_name
-        return entries[mid]
+        return @entries[mid]
       elsif name < mid_name
         upper = mid - 1
       elsif name > mid_name
@@ -57,6 +57,13 @@ class AddressBook
       end
     end
 
+    return nil
+  end
+
+  def iterative_search name
+    @entries.each do |entry|
+      return entry if entry.name == name
+    end
     return nil
   end
 
